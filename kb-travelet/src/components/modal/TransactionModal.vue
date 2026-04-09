762f64060156d6d22a6693b58e536304c58a724c
seoyeeon <shift2013@naver.com>
2026-04-09 13:38:01 +0900

    feat: #25 거래 데이터 구조 및 CRUD, 날짜 조회 구현

--- kb-travelet/src/components/modal/TransactionModal.vue
+++ kb-travelet/src/components/modal/TransactionModal.vue
@@ -1,5 +1,19 @@
-<template></template>
+<template>
+  <div v-if="store.transactions.length === 0">
+    거래 내역이 없습니다.
+  </div>
 
-<script setup></script>
+  <ul v-else>
+    <li v-for="t in store.transactions" :key="t.id">
+        {{ t.category }} - 
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { useAccountStore } from '@/stores/account';
+
+const store = useAccountStore();
+</script>
 
 <style scoped></style>
